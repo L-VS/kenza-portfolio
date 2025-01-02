adb156196bdf4c3a8e982b29a25d5caf85de4e56
L-VS <167241825+L-VS@users.noreply.github.com>
2025-01-02 21:48:30 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // Initialisation de l'API Google Drive
-    // Chargement des données et mise à jour du DOM
-  });
-  
+  // Initialisation de l'API Google Drive
+  // Chargement des données et mise à jour du DOM
+});
